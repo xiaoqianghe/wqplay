5f878ac187e0f1ed4c6f687679495f75899cffda
小强何 <1565706716@qq.com>
2017-06-15 22:18:46 +0800

    tijiao

--- app/src/main/java/com/xiaoqianghe/wqplay/presenter/RecommendPresenter.java
+++ app/src/main/java/com/xiaoqianghe/wqplay/presenter/RecommendPresenter.java
@@ -1,15 +1,24 @@
 package com.xiaoqianghe.wqplay.presenter;
 
+import android.support.annotation.MainThread;
+
 import com.xiaoqianghe.wqplay.bean.requestbean.AppInfo;
+import com.xiaoqianghe.wqplay.bean.requestbean.BaseBean;
 import com.xiaoqianghe.wqplay.bean.requestbean.PageBean;
 import com.xiaoqianghe.wqplay.data.RecommedModel;
 import com.xiaoqianghe.wqplay.presenter.contract.RecommendContract;
+
+import java.util.List;
 
 import javax.inject.Inject;
 
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
+import rx.Scheduler;
+import rx.Subscriber;
+import rx.android.schedulers.AndroidSchedulers;
+import rx.schedulers.Schedulers;
 
 /**
  * Author：Wq
@@ -25,7 +34,7 @@ public class RecommendPresenter extends BasePresenter<RecommedModel,RecommendCon
     }
 
     public void requestDatas(){
-        mView.showLoading();
+ //       mView.showLoading();
         //OkHttp中的的网络请求
 //        mModel.getApps(new Callback<PageBean<AppInfo>>(){
 //
@@ -46,6 +55,47 @@ public class RecommendPresenter extends BasePresenter<RecommedModel,RecommendCon
 //                mView.showError(t.getMessage());
 //            }
 //        });
+
+
+
+        //RxJava+Retrofit的网络请求的整合
+        mModel.getApps()
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new Subscriber<BaseBean<PageBean<AppInfo>>>() {
+
+            @Override
+            public void onStart() {
+                super.onStart();
+                mView.showLoading();
+            }
+
+            @Override
+            public void onCompleted() {
+                mView.dismissLoading();
+
+            }
+
+            @Override
+            public void onError(Throwable e) {
+                mView.dismissLoading();
+
+            }
+
+            @Override
+            public void onNext(BaseBean<PageBean<AppInfo>> pageBeanBaseBean) {
+
+                if(null!=pageBeanBaseBean){
+                    PageBean<AppInfo> mDatas=(PageBean<AppInfo>)pageBeanBaseBean.getData();
+                    mView.showResult((List<AppInfo>) mDatas);
+                }else{
+                    mView.showNodata();
+                }
+
+                mView.dismissLoading();
+
+            }
+        });
 
 
 
